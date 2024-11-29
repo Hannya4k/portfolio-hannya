fb7b3d8992c284147fbe2eddcf532290be953ca0
Karl <118022278+Hannya4k@users.noreply.github.com>
2024-11-29 22:24:43 +0800

    update cv

--- src/data/Contact.tsx
+++ src/data/Contact.tsx
@@ -3,5 +3,5 @@ interface ContactType {
 }
 
 export const Contact: ContactType = {
-  cv: "https://drive.google.com/file/d/1vfc2cgvB44ZWWnYXCpST9HwheEfyZb4I/view?usp=sharing",
+  cv: "https://drive.google.com/file/d/1bkBlhlH4plUt75xL633VpNjyrZWLX2H3/view?usp=sharing",
 };
