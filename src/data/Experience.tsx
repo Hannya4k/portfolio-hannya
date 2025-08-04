f9f4225bc6caf047e1282b0502c0488abe3b16f2
Karl <118022278+Hannya4k@users.noreply.github.com>
2025-08-05 07:57:26 +0800

    refactor Experience entries: update dates, enhance descriptions, and improve clarity

--- src/data/Experience.tsx
+++ src/data/Experience.tsx
@@ -11,18 +11,19 @@ export const Experience: ExperienceType[] = [
     title: "Cybersecurity Intern",
     subtitle: "Tech Advise Solutions and Services - Davao City, Philippines",
     website: "https://techadviseph.com/",
-    date: "May 2025 - Present",
+    date: "May - July 2025",
     content: {
       text: " ",
       list: [
-        "Conducted vulnerability assessment using Nessus and performed penetration testing on a website.",
-        "Create an EC2 instance and a security group using Terraform.",
+        "Conducted a vulnerability assessment using Nessus to identify and report potential security issues in web applications.",
+        "Performed basic penetration testing on a target website, simulating real-world attack scenarios.",
+        "Created an Amazon EC2 instance and configured a security group using Terraform to automate deployment of the test environment.",
       ],
       tech: [
-        "Red Team",
         "Cybersecurity",
-        "Ethical Hacking",
+        "Vulnerability Assessment",
         "Penetration Testing",
+        "Ethical Hacking",
         "IaC",
         "Terraform",
       ],
@@ -36,10 +37,11 @@ export const Experience: ExperienceType[] = [
     content: {
       text: " ",
       list: [
-        "Developed BetterMomsh, a pregnancy mobile app, using React Native.",
-        "Assisted in implementing the design for the Clinix Plus mobile app.",
-        "Implemented the design for the Compass mobile app and parts of the web platform.",
-        "Implemented design features for the ACS Web platform tailored for doctors.",
+        "Developed BetterMomsh, a mobile pregnancy app, using React Native.",
+        "Assisted in implementing UI components for the Clinix Plus mobile application.",
+        "Built core design features for the Compass mobile app and parts of its web platform.",
+        "Implemented custom front-end features for the ACS Web platform, tailored specifically for doctors’ workflows",
+        "Fixed bugs and added features to the PMRS website, improving functionality and user experience.",
       ],
       tech: [
         "React.js",
@@ -58,12 +60,12 @@ export const Experience: ExperienceType[] = [
     title: "Project Based Developer",
     subtitle: "Comcare Insurance Agency - Davao City, Philippines",
     // website: ' ',
-    date: "January 2024 - November 2024",
+    date: "January - November 2024",
     content: {
       text: "",
       list: [
-        "Helped debug issues and implemented requested features.",
-        "Developed the new Comcare website using Next.js.",
+        "Helped debug issues and implement requested features on a legacy system using PHP CodeIgniter.",
+        "Developed the new Comcare website using Next.js, focusing on modern, responsive design and performance optimization.",
       ],
       tech: [
         "PHP",
@@ -75,6 +77,7 @@ export const Experience: ExperienceType[] = [
         "SASS",
         "Next.js",
         "PosgreSQL",
+        "AntDesign",
       ],
     },
   },
