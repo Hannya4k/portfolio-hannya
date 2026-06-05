84ef46eec709f496eddcbdf72b126ee8fd1f37bc
Karl <118022278+Hannya4k@users.noreply.github.com>
2026-06-05 11:34:51 +0800

    add skeleton for images

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "../../styles/components/projectCard.module.scss";
 import { useScramble } from "../../hooks/useScramble";
 
@@ -13,10 +13,17 @@ interface ProjectCardProps {
 const ProjectCard: FC<ProjectCardProps> = ({ title, description, tech, image, featured = false }) => {
   const name = useScramble(title, 100, 100);
   const what = useScramble(description, 100, 200);
+  const [loaded, setLoaded] = useState(false);
   return (
     <div className={`${styles.projectCard} ${featured ? styles.featured : ''}`}>
       <div className={styles.imagewrapper}>
-        <img src={image} alt={title} />
+        {!loaded && <div className={styles.skeleton} />}
+        <img
+          src={image}
+          alt={title}
+          onLoad={() => setLoaded(true)}
+          className={loaded ? styles.img_loaded : styles.img_loading}
+        />
         <div className={styles.hover_overlay}>
           <span>[ view ]</span>
         </div>
