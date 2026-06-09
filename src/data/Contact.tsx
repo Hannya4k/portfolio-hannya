8a297ced8630b4c0ca6067fb1290d3e67bc38bf1
Karl <118022278+Hannya4k@users.noreply.github.com>
2026-06-09 16:48:14 +0800

    update cv

--- src/data/Contact.tsx
+++ src/data/Contact.tsx
@@ -5,7 +5,7 @@ interface ContactType {
 }
 
 export const Contact: ContactType = {
-  cv: "https://drive.google.com/file/d/17t7T1Y38SIHzb1oNMEhZTKO5RaOMfM1p/view?usp=sharing",
+  cv: "https://drive.google.com/file/d/1wFlcs_DDIkX-WChu3ngHymEEhQDwBE-b/view?usp=sharing",
   description:
     "Open to IT and software engineering opportunities. Let’s build something useful together.",
   email: "k.andoque07@gmail.com",
